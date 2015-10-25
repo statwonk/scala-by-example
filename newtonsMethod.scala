b2b3c7958d2252e0e08af0df26382460e424adf2
statwonk <cpeter9@gmail.com>
2015-10-25 01:22:49 -0500

    Make helpers private

--- newtonsMethod.scala
+++ newtonsMethod.scala
@@ -1,15 +1,19 @@
-def square(x: Double) = x * x
+def sqrt(x: Double) = {
 
-def abs(x: Double) = if (x >= 0) x else -x
+    def square(x: Double) = x * x
 
-def isGoodEnough(guess: Double, x: Double) =
-  abs(square(guess) - x) < 0.001
+    def abs(x: Double) = if (x >= 0) x else -x
 
-def improve(guess: Double, x: Double) =
-  (guess + (x / guess)) / 2
+    def isGoodEnough(guess: Double, x: Double) =
+      abs(square(guess) - x) < 0.001
 
-def sqrtIter(guess: Double, x: Double): Double =
-  if (isGoodEnough(guess, x)) guess else
-    sqrtIter(improve(guess, x), x)
+    def improve(guess: Double, x: Double) =
+      (guess + (x / guess)) / 2
 
-def sqrt(x: Double) = sqrtIter(1.0, x)
+    def sqrtIter(guess: Double, x: Double): Double =
+      if (isGoodEnough(guess, x)) guess else
+        sqrtIter(improve(guess, x), x)
+
+  sqrtIter(1.0, x)
+}
+
