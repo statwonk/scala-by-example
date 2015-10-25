e03215246376ac77e48bf361638b8746c4bf3764
statwonk <cpeter9@gmail.com>
2015-10-25 01:36:04 -0500

    Remove extraneous params available in scope

--- newtonsMethod.scala
+++ newtonsMethod.scala
@@ -1,19 +1,20 @@
+
+def square(x: Double) = x * x
+
+def abs(x: Double) = if (x >= 0) x else -x
+
 def sqrt(x: Double) = {
 
-    def square(x: Double) = x * x
-
-    def abs(x: Double) = if (x >= 0) x else -x
-
-    def isGoodEnough(guess: Double, x: Double) =
+    def isGoodEnough(guess: Double) =
       abs(square(guess) - x) < 0.001
 
-    def improve(guess: Double, x: Double) =
+    def improve(guess: Double) =
       (guess + (x / guess)) / 2
 
-    def sqrtIter(guess: Double, x: Double): Double =
-      if (isGoodEnough(guess, x)) guess else
-        sqrtIter(improve(guess, x), x)
+    def sqrtIter(guess: Double): Double =
+      if (isGoodEnough(guess)) guess else
+        sqrtIter(improve(guess))
 
-  sqrtIter(1.0, x)
+  sqrtIter(1.0)
 }
 
